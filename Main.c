352f4f1b8c491cbf49919b4da75ff4e72edc46f0
alvansa <alvarosotoaa@gmail.com>
2021-12-12 16:55:02 -0300

    asignar matriz

--- Main.c
+++ Main.c
@@ -3,7 +3,23 @@
 #include <stdlib.h>
 #include <time.h>
 
+long long **asignar_matriz(int n, int m);                                                // Reservar la memoria y crear una matriz nula
+
 int main()
 {
     return 0;
+}
+
+
+long long **asignar_matriz(int n, int m)
+{
+    int i, j;
+    long long **array;
+    array = (long long **)malloc(n * sizeof(long long *)); // se reserva memoria  para la matriz de x filas que contiene direcciones de memoria a las segundas dimensiones.
+    for (i = 0; i < n; i++)
+    {
+        array[i] = (long long *)malloc(m * sizeof(long long)); // se reserva memoria para las segundas dimensiones , x columnas
+    }
+    // en memoria ya tenemos reservado espacio para una matriz de x por x --> array[x][x]
+    return &*array; // retorno de un puntero doble
 }
